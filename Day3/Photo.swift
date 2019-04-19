77a332e9c1296c3bb26656aedaab36645932cc51
Jessica Mallian <jmallian@fastenal.com>
2019-04-19 12:51:18 -0500

    created Photo.swift

---
+++ Day3/Photo.swift
@@ -0,0 +1,23 @@
+//
+//  Photo.swift
+//  Day3
+//
+//  Created by Jessica Mallian on 4/19/19.
+//  Copyright © 2019 Jessica Mallian. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.remoteURL = remoteURL
+        self.photoID = photoID
+        self.dateTaken = dateTaken
+    }
+}
