00ab315ba94520a67c57a18d9c7c728105d61eae
Jessica Mallian <jmallian@fastenal.com>
2019-04-18 19:25:40 -0500

    created the interface

---
+++ Day3/PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  Day3
+//
+//  Created by Jessica Mallian on 4/18/19.
+//  Copyright © 2019 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView! 
+}
