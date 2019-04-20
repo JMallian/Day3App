71a4ae21e8e7392b324d44ece46b1580b5b4f228
Jessica Mallian <jlmallian@gmail.com>
2019-04-20 17:24:49 -0500

    created own UICollectionViewCell that shows an UIActivityInidcator

---
+++ Day3/PhotoCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Day3
+//
+//  Created by Jessica Mallian on 4/20/19.
+//  Copyright © 2019 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    // called when cell is first created
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+    
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
