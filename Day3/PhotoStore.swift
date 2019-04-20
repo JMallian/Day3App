b66ad63fe3035b21fdc92f2a84a1cd43e750abb2
Jessica Mallian <jlmallian@gmail.com>
2019-04-20 12:39:02 -0500

    can see a downloaded photo now

--- Day3/PhotoStore.swift
+++ Day3/PhotoStore.swift
@@ -6,11 +6,20 @@
 //  Copyright © 2019 Jessica Mallian. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum PhotosResult {
     case success([Photo])
     case failure(Error)
+}
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
 }
 
 /// This class is responsible for initiating the web service requests, it uses the URLSession API and the FlickrAPI struct to fetch a list of interesting photos and download the image data for each photo.
@@ -28,6 +37,29 @@ class PhotoStore {
             completion(result)
         }
         task.resume() 
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            let result = self.processImageRequest(data: data, error: error)
+            completion(result)
+        }
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
     }
     
     private func processPhotoRequest(data: Data?, error: Error?) -> PhotosResult {
