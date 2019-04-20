9ebe35ad188ef74473dca0fedae65560650b2d65
Jessica Mallian <jlmallian@gmail.com>
2019-04-20 17:06:40 -0500

    switched from using an UIImageView to show 1 image and started the work to use a UICollectionView instead

---
+++ Day3/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Day3
+//
+//  Created by Jessica Mallian on 4/20/19.
+//  Copyright © 2019 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        return cell 
+    }
+    
+    
+}
