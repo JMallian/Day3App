d51c16b06205c6f002380bc8d8df349718c00b10
Jessica Mallian <jlmallian@gmail.com>
2019-04-21 09:19:56 -0500

    created ImageStore

---
+++ Day3/ImageStore.swift
@@ -0,0 +1,58 @@
+//
+//  ImageStore.swift
+//  Day3
+//
+//  Created by Jessica Mallian on 4/21/19.
+//  Copyright © 2019 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class ImageStore {
+    let cache = NSCache<NSString, UIImage>()
+    
+    func setImage(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+        
+        // create a full URL for image
+        let url = imageURL(forKey: key)
+        
+        // turn image into JPEG data
+        if let data = image.jpegData(compressionQuality: 0.5) {
+            // write it it full URL
+            let _ = try? data.write(to: url, options: [.atomic])
+        }
+    }
+    
+    func image(forKey key: String) -> UIImage? {
+        if let existingImage = cache.object(forKey: key as NSString) {
+            return existingImage
+        }
+        
+        let url = imageURL(forKey: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key as NSString)
+        return imageFromDisk
+    }
+    
+    func deleteImage(forKey key: String) {
+        cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(forKey: key)
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let deleteError {
+            print("Error removing the image from disck: \(deleteError)")
+        }
+    }
+    
+    func imageURL(forKey key: String) -> URL {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        
+        return documentDirectory.appendingPathComponent(key)
+    }
+}
